9652d4cf4a67770424c21e5db2ac988a3654056e
anthony <ariberah@soe.uagrm.edu.bo>
2025-11-30 00:23:13 -0400

    Mejora base de datos

--- database/conexion.js
+++ database/conexion.js
@@ -7,6 +7,7 @@ const conexion = mysql.createPool({
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
+  port:process.env.PORT,
   multipleStatements: true,
 });
 
